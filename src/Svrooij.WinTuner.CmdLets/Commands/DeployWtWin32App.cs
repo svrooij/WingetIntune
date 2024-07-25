ddef6525f39440fe3dccfff0537374c56a2878c9
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2024-07-25 11:28:03 +0200

    Fixed auto update when superseeding
    Fixed #84

--- src/Svrooij.WinTuner.CmdLets/Commands/DeployWtWin32App.cs
+++ src/Svrooij.WinTuner.CmdLets/Commands/DeployWtWin32App.cs
@@ -245,7 +245,8 @@ public class DeployWtWin32App : BaseIntuneCmdlet
                 {
                     if (assignment.Intent == GraphModels.InstallIntent.Available && assignment.Settings is null)
                     {
-                        //assignment.Settings = new GraphModels.Win32LobAppAssignmentSettings { AutoUpdateSettings = new GraphModels.Win32LobAppAutoUpdateSettings { AutoUpdateSupersededApps = GraphModels.Win32LobAppAutoUpdateSupersededApps.Enabled } };
+                        assignment.Settings = new GraphModels.Win32LobAppAssignmentSettings { Notifications = GraphModels.Win32LobAppNotification.ShowReboot };
+                        assignment.Settings.AdditionalData.Add("autoUpdateSettings", new Win32LobAppAutoUpdateSettings());
                     }
                 }
 
