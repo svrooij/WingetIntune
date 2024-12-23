3b5d41263927e750ebf673713768956b29b2ab6f
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2024-12-23 16:12:24 +0100

    Update test

--- tests/WinTuner.Cmdlets.Tests/Show-MsiInfo.Tests.ps1
+++ tests/WinTuner.Cmdlets.Tests/Show-MsiInfo.Tests.ps1
@@ -1,3 +1,4 @@
+Import-Module -Force ".\src\Svrooij.WinTuner.CmdLets\bin\Debug\net6.0\Svrooij.WinTuner.CmdLets.dll"
 
 Describe 'Show-MsiInfo' {
     It 'Should be available' {
@@ -7,7 +8,8 @@ Describe 'Show-MsiInfo' {
 
     It 'Should download and show MSI info' {
         $msiUrl = 'https://download.microsoft.com/download/C/7/A/C7AAD914-A8A6-4904-88A1-29E657445D03/LAPS.x64.msi'
-        $msiInfo = Show-MsiInfo -MsiUrl $msiUrl -OutputPath $env:TEMP
+        $tempFolder = $env:TEMP
+        $msiInfo = $(Show-MsiInfo -MsiUrl $msiUrl -OutputPath $tempFolder)
         $msiInfo | Should -Not -BeNullOrEmpty
         $msiInfo.ProductCode | Should -Be '{97E2CA7B-B657-4FF7-A6DB-30ECC73E1E28}'
         $msiInfo.ProductVersion | Should -Be '6.2.0.0'
